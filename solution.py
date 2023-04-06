47369aea6da68676fe610f9addf48862acf6716a
AVladykina <55153864+AVladykina@users.noreply.github.com>
2023-04-05 23:16:08 -0700

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,5 @@ import numpy as np
 chat_id = 433242632 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    n = len(x)
-    lamp_sum = np.sum(x)
-    x = lamp_sum / (n * 40)
-    return x.mean() # Ваш ответ
+    
+    return x.mean()/40 # Ваш ответ
