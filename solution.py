ab6202deeb111624ca0fa4d41ff5eb5f1e07a0fe
AVladykina <55153864+AVladykina@users.noreply.github.com>
2023-04-05 23:12:24 -0700

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 433242632 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+    n = len(x)
+    lamp_sum = np.sum(x)
+    x = lamp_sum / (n * 40)
     return x.mean() # Ваш ответ
